2c4a509fe5d0f3e052bf069a9ea6aebd93c510dc
Sarah-Bue Bue <buettner-sarah@gmx.net>
2024-04-17 11:16:24 +0000

    feat: add event listeners for sound effects to buttons and cells when clicked

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,6 @@
                                     // HTML Elements
 const cells = document.querySelectorAll(".cell");
+const buttons = document.querySelectorAll(".button")
 const statusText = document.getElementById("status-text");
 const muteButton = document.getElementById("mute-button");
 const resetButton = document.getElementById("reset-button");
@@ -14,10 +15,22 @@ let gameRunning = false;
                                     // Sounds
 const clickSound = new Audio ("assets/audio/click.mp3");
 const victorySound = new Audio ("assets/audio/victory.mp3");
-
-console.log(clickSound)
-                                    
+const buttonSound = new Audio ("assets/audio/button.mp3")
 
                                     // Event Listeners
+cells.forEach((cell) => cell.addEventListener("click", playClick));
+buttons.forEach((button) => button.addEventListener("click", playButton));
+
 
                                     // Functions
+function playClick() {
+    clickSound.play();
+}
+
+function playButton() {
+    buttonSound.play();
+}
+
+function playVictory() {
+    victorySound.play();
+}
