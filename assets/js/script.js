c5e5d5cd8a290ff09e634effa96608e3429d7a67
Sarah-Bue Bue <buettner-sarah@gmx.net>
2024-04-17 13:30:50 +0000

    fix: update cells to include ids as index

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,8 @@
+// When page is loaded, set sound to unmute and game to start
+
                                     // HTML Elements
 const cells = document.querySelectorAll(".cell");
+const board = document.getElementById("board");
 const buttons = document.querySelectorAll(".button")
 const statusText = document.getElementById("status-text");
 const muteButton = document.getElementById("mute-button");
@@ -11,6 +14,16 @@ const player_X = "X";
 const player_O = "O";
 let currPlayer = player_X;
 let gameRunning = false;
+const winningCells = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+];
 
                                     // Sounds
 const clickSound = new Audio ("assets/audio/click.mp3");
@@ -26,7 +39,6 @@ buttons.forEach((button) => button.addEventListener("click", playButton));
 resetButton.addEventListener("click", startGame);
 muteButton.addEventListener("click", muteSound);
 gameModeButton.addEventListener("click", switchGameMode);
-
 
                                     // Functions
 function playClick() {
@@ -48,9 +60,6 @@ function muteSound() {
     buttonSound.muted = true;
     console.log("sound muted");
 }
-
-
-
 
 function switchGameMode() {
     // switch between pvp and pvc game mode
@@ -87,8 +96,9 @@ function checkDraw() {
 function announceWinner() {
     // announce who won
     // unhide status text
+    statusText.className = "visible";
+    console.log("you won")
 }
 
                                     // Start Game
 
-muteSound();
