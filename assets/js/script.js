77fb26ce32443616966b25ec285fee3a06aef4d8
Sarah-Bue Bue <buettner-sarah@gmx.net>
2024-04-17 10:50:59 +0000

    feat: create variables from html elements (buttons, cells, text) and player variables

---
+++ assets/js/script.js
@@ -0,0 +1,18 @@
+                                    // HTML Elements
+const cells = document.querySelectorAll(".cell");
+const statusText = document.getElementById("status-text");
+const muteButton = document.getElementById("mute-button");
+const resetButton = document.getElementById("reset-button");
+const gameModeButton = document.getElementById("game-mode-button");
+
+                                    // Game Elements
+const player_X = "X";
+const player_O = "O";
+let currPlayer = player_X;
+let gameRunning = false;
+
+                                    // Sounds
+
+                                    // Event Listeners
+
+                                    // Functions
